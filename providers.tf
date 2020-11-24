910cc9607abc609fda5aae30da74d3a1b7f65e8b
Schparky <3172830+Schparky@users.noreply.github.com>
2020-11-24 16:31:46 -0700

    move cloudflare version constraint back to the provider block
    running terraform locally seemed to want to look at both places for a version constraint

--- providers.tf
+++ providers.tf
@@ -5,6 +5,7 @@ provider "aws" {
 }
 
 provider "cloudflare" {
+  version    = "~> 2.0"
   email      = var.cloudflare_email
   api_key    = var.cloudflare_api_key
   api_token  = var.cloudflare_token
