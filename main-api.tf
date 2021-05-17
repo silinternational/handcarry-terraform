26c89c05c25b8d99e114d9efc38664b7e7439b12
Phillip <phillip.shipley@gmail.com>
2021-05-17 16:14:45 -0400

    pin postgres version to 11.11 for testing proxy

--- main-api.tf
+++ main-api.tf
@@ -85,6 +85,7 @@ module "rds" {
   app_name            = var.app_name
   app_env             = "${data.terraform_remote_state.common.outputs.app_env}-tf"
   engine              = "postgres"
+  engine_version      = "11.11"
   instance_class      = var.db_instance_class
   storage_encrypted   = var.db_storage_encrypted
   db_name             = var.db_database
