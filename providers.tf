e8d99fc80c68d284801e52c446fdaf2a996dfad0
Schparky <3172830+Schparky@users.noreply.github.com>
2023-04-19 16:52:15 +0800

    add AWS tags

--- providers.tf
+++ providers.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+
+  default_tags {
+    tags = {
+      managed_by        = "terraform"
+      workspace         = terraform.workspace
+      itse_app_env      = data.terraform_remote_state.common.outputs.app_environment
+      itse_app_customer = var.customer
+      itse_app_name     = "wecarry"
+    }
+  }
 }
 
 provider "cloudflare" {
